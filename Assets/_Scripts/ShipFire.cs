a224ca474166fe705d675f6567cabbb0e17118fc
Kenneth Zheng <kenzheng99@gmail.com>
2022-02-09 23:33:44 -0500

    add muzzle flash vfx

--- Assets/_Scripts/ShipFire.cs
+++ Assets/_Scripts/ShipFire.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.VFX;
 
 public class ShipFire : MonoBehaviour {
     [SerializeField] GameObject projectile;
     [SerializeField] Transform spawnLocation;
+    [SerializeField] private VisualEffect muzzleFlash;
+    
     [SerializeField] float projectileForce = 20;
     [SerializeField] float cooldownTime = .5f;
 
@@ -24,5 +27,6 @@ public class ShipFire : MonoBehaviour {
         Rigidbody projectileRB = projectileInstance.GetComponent<Rigidbody>();
         projectileRB.AddForce(transform.up * projectileForce, ForceMode.Impulse);
         lastFiredAt = Time.time;
+        muzzleFlash.Play();
     }
 }
